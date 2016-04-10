c40ac8aa8a8cefc6c18fa56911b142199f8795c3
Seth Hendrick <seth@shendrick.net>
2016-04-09 23:23:44 -0400

    Added ReadOnlyException.

---
+++ SethCS/Exceptions/ReadOnlyException.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SethCS.Exceptions
+{
+    /// <summary>
+    /// Exception that is thrown when something was being written
+    /// to when it shouldn't have been.
+    /// </summary>
+    public class ReadOnlyException : Exception
+    {
+        /// <summary>
+        /// Default Constructor.
+        /// </summary>
+        public ReadOnlyException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="message">Message that describes the error.</param>
+        public ReadOnlyException( string message ) :
+            base( message )
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
+        public ReadOnlyException( SerializationInfo info, StreamingContext context )
+            : base( info, context )
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="message">Message that describes the error.</param>
+        /// <param name="innerException">
+        /// The exception that is the cause of the current exception,
+        /// or a null reference (Nothing in Visual Basic) if no inner exception is specified.
+        /// </param>
+        public ReadOnlyException( string message, Exception innerException ) :
+            base( message, innerException )
+        {
+        }
+    }
+}
---
+++ Tests/Exceptions/ReadOnlyExceptionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using SethCS.Exceptions;
+
+namespace Tests.Exceptions
+{
+    [TestFixture]
+    public class ReadOnlyExceptionTest
+    {
+        /// <summary>
+        /// Ensures the exception gets constructed correctly.
+        /// </summary>
+        [Test]
+        public void ConstructorTest()
+        {
+            const string message = "msg";
+
+            // Ensure ex.Message becomes the message property.
+            ReadOnlyException ex = new ReadOnlyException( message );
+            Assert.AreEqual( message, ex.Message );
+
+            // Ensure ex.Message and ex.InnerException are passed in properly.
+            Exception inner = new Exception( "innerException" );
+            ex = new ReadOnlyException( message, inner );
+            Assert.AreEqual( message, ex.Message );
+            Assert.AreSame( inner, ex.InnerException );
+        }
+    }
+}
