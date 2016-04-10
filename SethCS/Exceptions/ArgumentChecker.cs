8debb8015138dbf68f6464e9fd1207b591a265d0
Seth Hendrick <seth@shendrick.net>
2016-04-09 21:46:50 -0400

    Added ArgumentChecker, a helper which checks to see if an argument is null or empty.
    If it is, it throws an ArgumentNullException.

---
+++ SethCS/Exceptions/ArgumentChecker.cs
@@ -0,0 +1,47 @@
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
+
+namespace SethCS.Exceptions
+{
+    /// <summary>
+    /// Helpers to ensure that arguments passed into a function
+    /// are valid.  Otherwise, ArgumentExceptions are thrown.
+    /// </summary>
+    public static class ArgumentChecker
+    {
+        /// <summary>
+        /// Ensures the given object is not null.  If it is,
+        /// this throws an ArgumentNullException.
+        /// </summary>
+        /// <param name="obj">The object to check</param>
+        /// <param name="argumentName">The name of the argument checked</param>
+        /// <exception cref="ArgumentNullException">Thrown if obj is null.</exception>
+        public static void IsNotNull( object obj, string argumentName )
+        {
+            if ( obj == null )
+            {
+                throw new ArgumentNullException( argumentName );
+            }
+        }
+
+        /// <summary>
+        /// Ensures the given string is not null or empty.  If it is,
+        /// this throws an ArgumentNullException.
+        /// </summary>
+        /// <param name="str">The string to check</param>
+        /// <param name="argumentName">The name of the argument checked</param>
+        /// <exception cref="ArgumentNullException">Thrown if obj is null.</exception>
+        public static void StringIsNotNullOrEmpty( string str, string argumentName )
+        {
+            if ( string.IsNullOrEmpty( str ) )
+            {
+                throw new ArgumentNullException( argumentName );
+            }
+        }
+    }
+}
---
+++ Tests/Exceptions/ArgumentCheckerTest.cs
@@ -0,0 +1,63 @@
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
+using NUnit.Framework;
+using SethCS.Exceptions;
+
+namespace Tests.Exceptions
+{
+    [TestFixture]
+    public class ArgumentCheckerTest
+    {
+        // -------- Fields --------
+
+        /// <summary>
+        /// The arg name to pass into the exception.
+        /// </summary>
+        private const string argName = "arg";
+
+        // -------- Tests --------
+
+        /// <summary>
+        /// Ensures the IsNotNull function works correctly.
+        /// </summary>
+        [Test]
+        public void IsNotNullTest()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>( () =>
+                ArgumentChecker.IsNotNull( null, argName )
+            );
+
+            Assert.AreEqual( argName, ex.ParamName );
+
+            Assert.DoesNotThrow( () =>
+                ArgumentChecker.IsNotNull( "Hello", argName )
+            );
+        }
+
+        /// <summary>
+        /// Ensures StringIsNotNullOrEmpty function works correctly.
+        /// </summary>
+        [Test]
+        public void StringIsNotNullOrEmptyTest()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>( () =>
+                ArgumentChecker.StringIsNotNullOrEmpty( null, argName )
+            );
+            Assert.AreEqual( argName, ex.ParamName );
+
+            ex = Assert.Throws<ArgumentNullException>( () =>
+                ArgumentChecker.StringIsNotNullOrEmpty( string.Empty, argName )
+            );
+            Assert.AreEqual( argName, ex.ParamName );
+
+            Assert.DoesNotThrow( () =>
+                ArgumentChecker.StringIsNotNullOrEmpty( "Hello", argName )
+            );
+        }
+    }
+}
