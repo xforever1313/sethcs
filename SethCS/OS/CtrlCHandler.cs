47e40314c678cd473b1e84c96a6d4997a6540e81
Seth Hendrick <seth@shendrick.net>
2016-05-05 22:16:00 -0400

    Added CTRL+C Handlers.  Untested (needs to be integrated with down-the-line applications for testing).

---
+++ SethCS/OS/CtrlCHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using SethCS.OS.Windows;
+
+namespace SethCS.OS
+{
+    /// <summary>
+    /// Handles when a user hit CTRL+C on Windows,
+    /// or sends a SIGTERM or SIGINT on linux.
+    /// </summary>
+    public abstract class CtrlCHandler : IDisposable
+    {
+        // -------- Fields --------
+
+        /// <summary>
+        /// The wait event that waits for the signal.
+        /// </summary>
+        protected ManualResetEvent signalEvent;
+
+        // -------- Constructor ---------
+
+        public static CtrlCHandler CreateHandler()
+        {
+#if __MonoCS__
+            if ( Environment.OSVersion.Platform == PlatformID.Unix )
+            {
+                reutrn new UnixCtrlCHandler();
+            }
+            else
+            {
+                return new WindowsCtrlCHandler();
+            }
+#else
+            return new WindowsCtrlCHandler();
+#endif
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        protected CtrlCHandler()
+        {
+            this.signalEvent = new ManualResetEvent( false );
+        }
+
+        // -------- Functions ---------
+
+        /// <summary>
+        /// Waits for the signal to occur.
+        /// </summary>
+        public void WaitForSignal()
+        {
+            this.signalEvent.WaitOne();
+        }
+
+        /// <summary>
+        /// Cleans up this class.
+        /// </summary>
+        public void Dispose()
+        {
+            CleanUp();
+        }
+
+        // ---- Abstract Functions ----
+
+        /// <summary>
+        /// Cleans up this class.
+        /// </summary>
+        protected abstract void CleanUp();
+    }
+}
---
+++ SethCS/OS/Unix/UnixCtrlCHandler.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+#if __MonoCS__
+using Mono.Unix;
+using Mono.Native;
+#endif
+
+namespace SethCS.OS.Unix
+{
+#if __MonoCS__
+
+    public class UnixCtrlCHandler : CtrlCHandler
+    {
+        // -------- Fields --------
+
+        /// <summary>
+        /// Signals to watch for to terminate the program.
+        /// </summary>
+        private static readonly UnixSignal[] signalsToWatch = {
+            new UnixSignal( Signum.SIGTERM ), // Termination Signal
+            new UnixSignal( Signum.SIGINT )   // Interrupt from the keyboard
+        };
+
+        Thread signalThread;
+
+        // -------- Constructor --------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <exception cref="PlatformNotSupportedException">Thrown when ran on windows.</exception>
+        public UnixCtrlCHandler() :
+            base()
+        {
+            if ( Environment.OSVersion.Platform != PlatformID.Unix )
+            {
+                throw new PlatformNotSupportedException(
+                    "Unix does not support WindowsCtrlCHandler!"
+                );
+            }
+
+            this.signalThread = new Thread(
+                delegate()
+                {
+                    try
+                    {
+                        UnixSignal.WaitAny( signalsToWatch );
+                        this.signalEvent.Set();
+                    }
+                    catch( ThreadAbortException )
+                    {
+                    }
+                }
+            );
+
+            this.signalThread.Start();
+        }
+
+        // -------- Functions --------
+
+        /// <summary>
+        /// Cleans up this class.
+        /// </summary>
+        protected override void CleanUp()
+        {
+            if ( this.signalThread.IsAlive )
+            {
+                this.signalThread.Abort();
+                this.signalThread.Join();
+            }
+        }
+
+    }
+
+#else
+
+    public class UnixCtrlCHandler : CtrlCHandler
+    {
+        // -------- Constructor --------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <exception cref="PlatformNotSupportedException">Thrown when ran on windows.</exception>
+        public UnixCtrlCHandler() :
+            base()
+        {
+            throw new PlatformNotSupportedException(
+                "Windows can not use the UnixCtrlCHandler!"
+            );
+        }
+
+        // -------- Functions --------
+
+        /// <summary>
+        /// Cleans up this class.
+        /// </summary>
+        protected override void CleanUp()
+        {
+
+        }
+    }
+#endif
+
+}
