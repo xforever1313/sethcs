1f524b4cf5a8c3df1726a92e13f7e85f12810ebc
Seth Hendrick <seth@shendrick.net>
2016-09-08 22:09:42 -0400

    Added ValidationException.

---
+++ SethCS/Exceptions/ValidationException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SethCS.Exceptions
+{
+    /// <summary>
+    /// Exception that is thrown when something isn't valid.
+    /// </summary>
+    public class ValidationException : Exception
+    {
+        /// <summary>
+        /// Default Constructor.
+        /// </summary>
+        public ValidationException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="message">Message that describes the error.</param>
+        public ValidationException( string message ) :
+            base( message )
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
+        public ValidationException( SerializationInfo info, StreamingContext context )
+            : base( info, context )
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="message">Message that describes the error.</param>
+        /// <param name="innerException">
+        /// The exception that is the cause of the current exception,
+        /// or a null reference (Nothing in Visual Basic) if no inner exception is specified.
+        /// </param>
+        public ValidationException( string message, Exception innerException ) :
+            base( message, innerException )
+        {
+        }
+    }
+}
---
+++ Tests/Exceptions/ValidationExceptionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using SethCS.Exceptions;
+
+namespace Tests.Exceptions
+{
+    [TestFixture]
+    public class ValidationExceptionTest
+    {
+        /// <summary>
+        /// Ensures the exception gets constructed correctly.
+        /// </summary>
+        [Test]
+        public void ConstructorTest()
+        {
+            const string message = "msg";
+
+            // Ensure ex.Message becomes the message property.
+            ValidationException ex = new ValidationException( message );
+            Assert.AreEqual( message, ex.Message );
+
+            // Ensure ex.Message and ex.InnerException are passed in properly.
+            Exception inner = new Exception( "innerException" );
+            ex = new ValidationException( message, inner );
+            Assert.AreEqual( message, ex.Message );
+            Assert.AreSame( inner, ex.InnerException );
+        }
+    }
+}
