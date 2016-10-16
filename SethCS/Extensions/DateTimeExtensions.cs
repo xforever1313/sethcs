ef29625c1aedbc7aec6d89647db1c39103f466aa
Seth Hendrick <seth@shendrick.net>
2016-10-16 16:31:08 -0400

    Added DateTime Extension methods.

---
+++ SethCS/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,35 @@
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
+
+namespace SethCS.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Returns a string of the given timestamp in the form of ISO 8601
+        /// The same as DateTime.ToString("o");
+        /// </summary>
+        /// <param name="timeStamp">Time stamp to get string of.</param>
+        /// <returns>String representation of the timestamp in ISO 8601 format.</returns>
+        public static string ToTimeStampString( this DateTime timeStamp )
+        {
+            return timeStamp.ToString( "O" );
+        }
+
+        /// <summary>
+        /// Returns a string of the given timestamp that works well with file names.
+        /// yyyy-MM-dd_HH-mm-ss-ffff
+        /// </summary>
+        /// <param name="timeStamp">Time stamp to get string of.</param>
+        /// <returns>A string in the form of yyyy-MM-dd_HH-mm-ss-ffff</returns>
+        public static string ToFileNameString( this DateTime timeStamp )
+        {
+            return timeStamp.ToString( "yyyy-MM-dd_HH-mm-ss-ffff" );
+        }
+    }
+}
