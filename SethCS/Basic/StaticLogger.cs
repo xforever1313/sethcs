2593677fdd8e2ba43e404837e0b2318f4087c2aa
Seth Hendrick <seth@shendrick.net>
2017-02-19 16:19:12 -0500

    Added Static Logger.

---
+++ SethCS/Basic/StaticLogger.cs
@@ -0,0 +1,62 @@
+﻿
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
+
+namespace SethCS.Basic
+{
+    public static class StaticLogger
+    {
+        // ---------------- Fields ----------------
+
+        /// <summary>
+        /// Event that is triggered when WriteLine is called.
+        /// 
+        /// Does not need to be thread-safe, all functions in this class are.
+        /// </summary>
+        public static event Action<string> OnWriteLine;
+
+        private static object onWriteLock = new object();
+
+        // ---------------- Functions ----------------
+        
+        /// <summary>
+        /// Writes an empty line to the listening events.
+        /// 
+        /// Thread-Safe.
+        /// </summary>
+        public static void WriteLine()
+        {
+            WriteLine( string.Empty );
+        }
+
+        /// <summary>
+        /// Writes a formatted string to the listening events.
+        /// </summary>
+        /// <param name="formatStr">The format string.</param>
+        /// <param name="objects">The objects used to format the string.</param>
+        public static void WriteLine( string formatStr, params object[] objects )
+        {
+            WriteLine( string.Format( formatStr, objects ) );
+        }
+
+        /// <summary>
+        /// Writes a string with a new line at the end
+        /// to all the events.
+        /// 
+        /// Thread-Safe.
+        /// </summary>
+        /// <param name="line">The string to write.</param>
+        public static void WriteLine( string line )
+        {
+            lock( onWriteLock )
+            {
+                Action<string> action = OnWriteLine;
+                action?.Invoke( line + Environment.NewLine );
+            }
+        }
+    }
+}
---
+++ Tests/Basic/StaticLoggerTest.cs
@@ -0,0 +1,69 @@
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
+using System.Text;
+using NUnit.Framework;
+using SethCS.Basic;
+
+namespace Tests.Basic
+{
+    public class StaticLoggerTest
+    {
+        // ---------------- Fields ----------------
+
+        private StringBuilder loggedMessages;
+
+        // ---------------- Setup / Teardown ----------------
+
+        [SetUp]
+        public void TestSetup()
+        {
+            this.loggedMessages = new StringBuilder();
+            StaticLogger.OnWriteLine += StaticLogger_OnWriteLine;
+        }
+
+        [TearDown]
+        public void TestTeardown()
+        {
+            StaticLogger.OnWriteLine -= StaticLogger_OnWriteLine;
+        }
+
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void WriteEmptyLineTest()
+        {
+            StaticLogger.WriteLine();
+            Assert.AreEqual( string.Empty + Environment.NewLine, this.loggedMessages.ToString() );
+        }
+
+        [Test]
+        public void WriteLineTest()
+        {
+            const string expectedString = "Hello, World!";
+            StaticLogger.WriteLine( expectedString );
+            Assert.AreEqual( expectedString + Environment.NewLine, this.loggedMessages.ToString() );
+        }
+
+        [Test]
+        public void WriteLineFormatTest()
+        {
+            const string formatString = "{0} + {1} = {2}";
+            string expectedString = "1 + 2 = 3" + Environment.NewLine;
+
+            StaticLogger.WriteLine( formatString, 1, 2, 3 );
+            Assert.AreEqual( expectedString, this.loggedMessages.ToString() );
+        }
+
+        // ---------------- Test Helpers ----------------
+
+        private void StaticLogger_OnWriteLine( string line )
+        {
+            this.loggedMessages.Append( line );
+        }
+    }
+}
