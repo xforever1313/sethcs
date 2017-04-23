27ab82b0280833b69d0acd51d8d74dcdb16fc2bc
Seth Hendrick <seth@shendrick.net>
2017-04-23 17:16:30 -0400

    Added string extensions.

---
+++ SethCS/Extensions/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Text.RegularExpressions;
+
+namespace SethCS.Extensions
+{
+    /// <summary>
+    /// Extensions for strings.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Replaces all white space with the given single character.
+        /// Includes tabs and new lines.
+        /// 
+        /// If there are multiple whitespaces in a row, it is only replaced
+        /// with the single character.
+        /// </summary>
+        /// <example>
+        /// Hello    World
+        /// How are you?
+        /// 
+        /// becomes (if ch is '_')
+        /// Hello_World_How_are_you?
+        /// </example>
+        public static string NormalizeWhiteSpace( this string str, char replaceCharacter = ' ' )
+        {
+            Regex regex = new Regex( @"\s+" );
+            return regex.Replace( str, "" + replaceCharacter );
+        }
+    }
+}
---
+++ Tests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using NUnit.Framework;
+using SethCS.Extensions;
+
+namespace Tests.Extensions
+{
+    [TestFixture]
+    public class StringExtensionsTest
+    {
+        [Test]
+        public void NormalizeWhiteSpaceTest()
+        {
+            string startString = "Hello\t  \nWorld, how\nare\tyou?I am fine!";
+            string expectedString = "Hello_World,_how_are_you?I_am_fine!";
+
+            string actualString = startString.NormalizeWhiteSpace( '_' );
+
+            Assert.AreEqual( expectedString, actualString );
+        }
+    }
+}
