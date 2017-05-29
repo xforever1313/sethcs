6139811c72d2c4d77261e2b0270a8e825fb5f8ca
Seth Hendrick <seth@shendrick.net>
2017-05-29 18:59:57 -0400

    Added a function that takes a path and converts it to a URL that can be opened with an HTTP request.

---
+++ SethCS/Extensions/Path.cs
@@ -0,0 +1,24 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+
+namespace SethCS.Extensions
+{
+    public static class SethPath
+    {
+        /// <summary>
+        /// Takes a path to the given file and returns
+        /// a URI that can be used in a browser or an HTTP request (file://).
+        /// </summary>
+        /// <param name="path">The path to convert.</param>
+        public static string ToUri( string path )
+        {
+            return new Uri( new Uri( "file://" ), path ).AbsoluteUri;
+        }
+    }
+}
---
+++ Tests/Extensions/PathTests.cs
@@ -0,0 +1,34 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using NUnit.Framework;
+using SethCS.Extensions;
+
+namespace Tests.Extensions
+{
+    [TestFixture]
+    public class PathTests
+    {
+        [Test]
+        public void ToUriTests()
+        {
+            {
+                string path = @"D:\Seth\source\Chaskis\Chaskis\CodeCoverage\index.htm";
+                string expectedPath = @"file:///D:/Seth/source/Chaskis/Chaskis/CodeCoverage/index.htm";
+
+                Assert.AreEqual( expectedPath, SethPath.ToUri( path ) );
+            }
+
+            {
+                string path = "/home/seth/Downloads/canvas.png";
+                string expectedPath = @"file:///home/seth/Downloads/canvas.png";
+
+                Assert.AreEqual( expectedPath, SethPath.ToUri( path ) );
+            }
+        }
+    }
+}
