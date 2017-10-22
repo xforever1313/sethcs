5a2b1c7c2321a20fbd982c60b34c012f480de40d
Seth Hendrick <seth@shendrick.net>
2017-10-22 16:38:42 -0400

    Created HttpGetter, which downloads strings from the internet.

---
+++ SethCS/IO/HttpGetter.cs
@@ -0,0 +1,75 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SethCS.IO
+{
+    /// <summary>
+    /// This class downloads the string of a given URL and returns it.
+    /// </summary>
+    public static class HttpGetter
+    {
+        // ----------------- Fields -----------------
+
+        public const int DefaultTimeout = 30 * 1000;
+
+        public const string DefaultUserAgent = "SethCS HttpGetter";
+
+        // ----------------- Constructor -----------------
+
+        /// <summary>
+        /// Downloads the string content from the given URL.
+        /// Encoded as UTF-8... This also means that downloading binaries will give you garbage.
+        /// </summary>
+        /// <param name="url">The URL to download from.</param>
+        /// <param name="userAgent">The user agent to use, if null we use the default.</param>
+        /// <param name="timeout">How long to wait before giving up.  Default is 30 seconds.</param>
+        /// <returns>The string that was downloaded.</returns>
+        public static string DownloadString( string url, string userAgent = DefaultUserAgent, int timeout = DefaultTimeout )
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create( url );
+            request.Method = "GET";
+            request.Timeout = timeout;
+            request.UserAgent = userAgent ?? "SethCS HttpGetter";
+
+            string str;
+
+            using( HttpWebResponse response = (HttpWebResponse)request.GetResponse() )
+            {
+                if( response.StatusCode != HttpStatusCode.OK )
+                {
+                    throw new WebException(
+                        "Did not get OK status code.  Got " + response.StatusCode + " " + response.StatusDescription
+                    );
+                }
+
+                using( StreamReader reader = new StreamReader( response.GetResponseStream(), Encoding.UTF8 ) )
+                {
+                    str = reader.ReadToEnd();
+                }
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Downloads the string content from the given URL in a background thread.
+        /// </summary>
+        /// <param name="url">The URL to download from.</param>
+        /// <param name="userAgent">The user agent to use, if null we use the default.</param>
+        /// <param name="timeout">How long to wait before giving up.</param>
+        /// <returns>The string that was downloaded.</returns>
+        public static Task<string> AsyncDownloadString( string url, string userAgent = DefaultUserAgent, int timeout = DefaultTimeout )
+        {
+            return Task.Run( () => DownloadString( url, userAgent, timeout ) );
+        }
+    }
+}
---
+++ Tests/IO/HttpGetterTest.cs
@@ -0,0 +1,97 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SethCS.IO;
+
+namespace Tests.IO
+{
+    [TestFixture]
+    public class HttpGetterTest
+    {
+        // ---------------- Fields ----------------
+
+        // ---------------- Setup / Teardown ----------------
+
+        [OneTimeSetUp]
+        public void FixtureSetup()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void FixtureTeardown()
+        {
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+        }
+
+        [TearDown]
+        public void TestTeardown()
+        {
+        }
+
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void GetTest()
+        {
+            string url = "https://files.shendrick.net/projects/sethcs/tests/test.txt";
+
+            string str = HttpGetter.DownloadString( url ).TrimEnd();
+            Assert.AreEqual( "Hello, World!", str );
+        }
+
+        /// <summary>
+        /// Ensures if we get a binary, we don't crash too badly.
+        /// </summary>
+        [Test]
+        public void GetBinaryTest()
+        {
+            string url = "https://files.shendrick.net/projects/sethcs/tests/test.png";
+
+            Assert.DoesNotThrow( () => HttpGetter.DownloadString( url ) );
+        }
+
+        [Test]
+        public void GetAsyncTest()
+        {
+            string url = "https://files.shendrick.net/projects/sethcs/tests/test.txt";
+
+            Task<string> strTask = HttpGetter.AsyncDownloadString( url );
+            strTask.Wait();
+
+            string str = strTask.Result.TrimEnd();
+            Assert.AreEqual( "Hello, World!", str );
+        }
+
+        [Test]
+        public void Get404Test()
+        {
+            string url = "https://files.shendrick.net/projects/sethcs/dne.txt";
+
+            WebException e = Assert.Throws<WebException>( () => HttpGetter.DownloadString( url ) );
+            Assert.IsTrue( e.Message.Contains( Convert.ToInt32( HttpStatusCode.NotFound ).ToString() ) );
+        }
+
+        [Test]
+        public void TimeoutTest()
+        {
+            string url = "http://127.0.0.0:13131"; // Hopefully this isn't in use...
+
+            WebException e = Assert.Throws<WebException>( () => HttpGetter.DownloadString( url, timeout:10 ) );
+            Assert.IsTrue( e.Message.Contains( "timed out" ) );
+        }
+
+        // ---------------- Test Helpers ----------------
+    }
+}
