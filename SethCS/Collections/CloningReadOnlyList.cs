ffa279de61d7e555d7fe63451d0156803be83fde
Seth Hendrick <seth@shendrick.net>
2017-11-29 23:14:25 -0500

    Added Cloning ReadOnlyList.

---
+++ SethCS/Collections/CloningReadOnlyList.cs
@@ -0,0 +1,164 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SethCS.Collections
+{
+    /// <summary>
+    /// This class is meant to be a base class.
+    /// Its purpose is to treat the parent class as a list,
+    /// but when a caller accesses an element, they ALWAYS receive a clone,
+    /// so the underlying data is never modified.
+    /// </summary>
+    public abstract class BaseCloningReadOnlyList<T> : IReadOnlyList<T>
+    {
+        // ---------------- Fields ----------------
+
+        protected List<T> list;
+
+        // ---------------- Constructor ----------------
+
+        protected BaseCloningReadOnlyList()
+        {
+            this.list = new List<T>();
+        }
+
+        // ---------------- Properties ----------------
+
+        // -------- IReadOnlyList Implementation --------
+
+        /// <summary>
+        /// Returns a copy of the object at the given index.
+        /// </summary>
+        public T this[int index]
+        {
+            get
+            {
+                return this.CloneInstructions( this.list[index] );
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.list.Count;
+            }
+        }
+
+        // ---------------- Functions ----------------
+
+        /// <summary>
+        /// Implementors must have this return a clone of the original
+        /// object.  That is, the returned object is equivalent to the original,
+        /// but they are NOT the same reference.
+        /// </summary>
+        protected abstract T CloneInstructions( T original );
+
+        // -------- IEnumerable Implementation --------
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CloningReadOnlyListEnumerator( this );
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        // ---------------- Helper Classes ----------------
+
+        private class CloningReadOnlyListEnumerator : IEnumerator<T>
+        {
+            // ---------------- Fields ----------------
+
+            private BaseCloningReadOnlyList<T> cloningList;
+            int position;
+
+            // ---------------- Constructor ----------------
+
+            public CloningReadOnlyListEnumerator( BaseCloningReadOnlyList<T> cloningList )
+            {
+                this.cloningList = cloningList;
+
+                // Enumerators are positioned before the first element
+                // until the first MoveNext() call.
+                this.position = -1;
+            }
+
+            // ---------------- Properties ----------------
+
+            // -------- IEnumerator Implementation --------
+
+            public T Current
+            {
+                get
+                {
+                    return this.cloningList.CloneInstructions( this.cloningList.list[this.position] );
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return this.Current;
+                }
+            }
+
+            // ---------------- Functions ----------------
+
+            // -------- IEnumerator Implementation --------
+
+            public void Dispose()
+            {
+                // Nothing to do.
+            }
+
+            public bool MoveNext()
+            {
+                ++this.position;
+                return this.position < this.cloningList.list.Count;
+            }
+
+            public void Reset()
+            {
+                this.position = -1;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This is a version of the CloningReadOnlyList, where instead of inheriting from
+    /// BaseCloningReadOnlyList, the cloning action is passed in as a parameter during
+    /// construction.
+    /// </summary>
+    public class CloningReadOnlyList<T> : BaseCloningReadOnlyList<T>
+    {
+        // ---------------- Fields ----------------
+
+        private Func<T, T> cloningAction;
+
+        // ---------------- Constructor ----------------
+
+        public CloningReadOnlyList( Func<T, T> cloningAction )
+        {
+            this.cloningAction = cloningAction;
+        }
+
+        // ---------------- Functions ----------------
+
+        protected override T CloneInstructions( T original )
+        {
+            return cloningAction( original );
+        }
+    }
+}
---
+++ Tests/Collections/CloningReadOnlyListTests.cs
@@ -0,0 +1,125 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SethCS.Collections;
+
+namespace Tests.Collections
+{
+    public class CloneableClass
+    {
+        public CloneableClass( int value )
+        {
+            this.Value = value;
+        }
+
+        public int Value { get; private set; }
+
+        public CloneableClass Clone()
+        {
+            return (CloneableClass)this.MemberwiseClone();
+        }
+    }
+
+    public class CloneList : BaseCloningReadOnlyList<CloneableClass>
+    {
+        public void Add( CloneableClass value )
+        {
+            this.list.Add( value );
+        }
+
+        protected override CloneableClass CloneInstructions( CloneableClass original )
+        {
+            return original.Clone();
+        }
+    }
+
+    [TestFixture]
+    public class CloningReadOnlyListTests
+    {
+        // ---------------- Fields ----------------
+
+        private CloneableClass obj1;
+        private CloneableClass obj2;
+        private CloneableClass obj3;
+
+        private CloneList uut;
+
+        // ---------------- Setup / Teardown ----------------
+
+        [OneTimeSetUp]
+        public void FixtureSetup()
+        {
+            this.obj1 = new CloneableClass( 1 );
+            this.obj2 = new CloneableClass( 2 );
+            this.obj3 = new CloneableClass( 3 );
+
+            this.uut = new CloneList();
+
+            this.uut.Add( obj1 );
+            this.uut.Add( obj2 );
+            this.uut.Add( obj3 );
+        }
+
+        [OneTimeTearDown]
+        public void FixtureTeardown()
+        {
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+        }
+
+        [TearDown]
+        public void TestTeardown()
+        {
+        }
+
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void CountTest()
+        {
+            Assert.AreEqual( 3, this.uut.Count );
+        }
+
+        [Test]
+        public void IndexTest()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>( () => { int x = this.uut[-1].Value; } );
+            Assert.Throws<ArgumentOutOfRangeException>( () => { int x = this.uut[3].Value; } );
+
+            Assert.AreEqual( 1, this.uut[0].Value );
+            Assert.AreEqual( 2, this.uut[1].Value );
+            Assert.AreEqual( 3, this.uut[2].Value );
+
+            Assert.AreNotSame( this.obj1, this.uut[0] );
+            Assert.AreNotSame( this.obj2, this.uut[1] );
+            Assert.AreNotSame( this.obj3, this.uut[2] );
+        }
+
+        [Test]
+        public void ForEachTest()
+        {
+            int index = 0;
+            CloneableClass[] originals = new CloneableClass[] { obj1, obj2, obj3 };
+
+            foreach( CloneableClass c in this.uut )
+            {
+                Assert.AreEqual( originals[index].Value, c.Value );
+                Assert.AreNotSame( originals[index], c );
+                ++index;    
+            }
+        }
+
+        // ---------------- Test Helpers ----------------
+    }
+}
