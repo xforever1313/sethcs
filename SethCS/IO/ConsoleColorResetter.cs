b05e7a8b3235ae09ae213e1ed5d26832009eebe7
Seth Hendrick <seth@shendrick.net>
2018-11-25 16:13:19 -0500

    Added unit tests to ConsoleColorResetter.

--- SethCS/IO/ConsoleColorResetter.cs
+++ SethCS/IO/ConsoleColorResetter.cs
@@ -5,14 +5,19 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
 
+using System;
+
 namespace SethCS.IO
 {
 
     /// <summary>
     /// Helper class that changes the console color and then puts it back
     /// when Dispose() is called.
+    /// 
+    /// This is designed like C++'s RAII, where the Constructor sets the colors,
+    /// and Dispose() resets them when the variable goes out-of-scope in a using statement.
     /// </summary>
-    public class ConsoleColorReseter : IDisposable
+    public class ConsoleColorResetter : IDisposable
     {
         // ---------------- Fields ----------------
 
@@ -21,7 +26,12 @@ namespace SethCS.IO
 
         // ---------------- Constructor ----------------
 
-        public ConsoleColorReseter( ConsoleColor? newForeground, ConsoleColor? newBackground )
+        /// <summary>
+        /// Constructor - sets the background colors.
+        /// </summary>
+        /// <param name="newForeground">The new foreground (text) color of the console.  Null to NOT change it.</param>
+        /// <param name="newBackground">The new background color of the console.  Null to NOT change it.</param>
+        public ConsoleColorResetter( ConsoleColor? newForeground, ConsoleColor? newBackground )
         {
             if( newForeground != null )
             {
---
+++ Tests/IO/ConsoleColorResetterTests.cs
@@ -0,0 +1,85 @@
+﻿//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using NUnit.Framework;
+using SethCS.IO;
+
+namespace Tests.IO
+{
+    [TestFixture]
+    public class ConsoleColorResetterTests
+    {
+        // ---------------- Fields ----------------
+
+        private ConsoleColor originalForeground;
+
+        private ConsoleColor originalBackground;
+
+        // ---------------- Setup / Teardown ----------------
+
+        [SetUp]
+        public void TestSetup()
+        {
+            this.originalBackground = Console.BackgroundColor;
+            this.originalForeground = Console.ForegroundColor;
+        }
+
+        [TearDown]
+        public void TestTeardown()
+        {
+        }
+
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void BackgroundOnlyTest()
+        {
+            const ConsoleColor newBgColor = ConsoleColor.DarkGreen;
+            using( ConsoleColorResetter uut = new ConsoleColorResetter( null, newBgColor ) )
+            {
+                Assert.AreEqual( originalForeground, Console.ForegroundColor );
+                Assert.AreEqual( newBgColor, Console.BackgroundColor );
+            }
+
+            // Should be restored after dispose is called.
+            Assert.AreEqual( originalForeground, Console.ForegroundColor );
+            Assert.AreEqual( originalBackground, Console.BackgroundColor );
+        }
+
+        [Test]
+        public void ForegroundOnlyTest()
+        {
+            const ConsoleColor newFgColor = ConsoleColor.DarkGreen;
+            using( ConsoleColorResetter uut = new ConsoleColorResetter( newFgColor, null ) )
+            {
+                Assert.AreEqual( newFgColor, Console.ForegroundColor );
+                Assert.AreEqual( originalBackground, Console.BackgroundColor );
+            }
+
+            // Should be restored after dispose is called.
+            Assert.AreEqual( originalForeground, Console.ForegroundColor );
+            Assert.AreEqual( originalBackground, Console.BackgroundColor );
+        }
+
+        [Test]
+        public void BackgroundAndForegroundTest()
+        {
+            const ConsoleColor newFgColor = ConsoleColor.DarkGreen;
+            const ConsoleColor newBgColor = ConsoleColor.DarkMagenta;
+            using( ConsoleColorResetter uut = new ConsoleColorResetter( newFgColor, newBgColor ) )
+            {
+                Assert.AreEqual( newFgColor, Console.ForegroundColor );
+                Assert.AreEqual( newBgColor, Console.BackgroundColor );
+            }
+
+            // Should be restored after dispose is called.
+            Assert.AreEqual( originalForeground, Console.ForegroundColor );
+            Assert.AreEqual( originalBackground, Console.BackgroundColor );
+        }
+    }
+}
