e790db1dd5b80bba30b26263c7650209be634f33
Seth Hendrick <seth@shendrick.net>
2019-05-25 16:47:43 -0400

    Added IEnumerableExtensions and ListedValidationException.

---
+++ SethCS/Exceptions/ListedValidationException.cs
@@ -0,0 +1,41 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using SethCS.Extensions;
+
+namespace SethCS.Exceptions
+{
+    /// <summary>
+    /// This is a <seealso cref="ValidationException"/>, but allows for a list of
+    /// error messages instead of a single one.
+    /// </summary>
+    public class ListedValidationException : ValidationException
+    {
+        // ---------------- Constructor ----------------
+
+        public ListedValidationException( string context, IEnumerable<string> errors ) :
+            base( context + Environment.NewLine + errors.ToListString( " - " ) )
+        {
+            this.Context = context;
+            this.Errors = errors;
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// The reason why this exception was thrown.
+        /// </summary>
+        public string Context { get; private set; }
+
+        /// <summary>
+        /// Collection of errors that caused the exception.
+        /// </summary>
+        public IEnumerable<string> Errors { get; private set; }
+    }
+}
---
+++ SethCS/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,42 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SethCS.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Iterates through each element in the Enumerable and calls ToString() on each element,
+        /// putting the output on a newline for each element in the list.
+        /// </summary>
+        /// <param name="startCharacter">What the start character of each line should be (e.g. "- ").  Defaults to none (null).</param>
+        public static string ToListString<T>( this IEnumerable<T> list, string startCharacter = null )
+        {
+            if ( startCharacter == null )
+            {
+                startCharacter = string.Empty;
+            }
+
+            StringBuilder builder = new StringBuilder();
+            foreach ( T l in list )
+            {
+                builder.AppendLine( startCharacter + l.ToString() );
+            }
+
+            return builder.ToString();
+        }
+
+        public static bool IsEmpty<T>( this IEnumerable<T> list )
+        {
+            return list.LongCount() == 0;
+        }
+    }
+}
