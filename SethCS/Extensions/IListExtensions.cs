a043867c0fc2c2183424a471dbf99ea308505436
Seth Hendrick <seth@shendrick.net>
2021-01-30 15:34:38 -0500

    Added IListExtensions.

---
+++ SethCS/Extensions/IListExtensions.cs
@@ -0,0 +1,24 @@
+﻿//
+//          Copyright Seth Hendrick 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Collections.Generic;
+
+namespace SethCS.Extensions
+{
+    public static class IListExtensions
+    {
+        // ---------------- Functions ----------------
+
+        public static void AddRange<T>( this IList<T> list, IEnumerable<T> valuesToAdd )
+        {
+            foreach( T value in valuesToAdd )
+            {
+                list.Add( value );
+            }
+        }
+    }
+}
