11c99d68fdebbbbf78921c93767fa2c7f85e558b
Seth Hendrick <seth@shendrick.net>
2021-03-06 13:04:50 -0500

    Add DirectoryHelpers class.

---
+++ SethCS/IO/DirectoryHelpers.cs
@@ -0,0 +1,22 @@
+﻿//
+//          Copyright Seth Hendrick 2016-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.IO;
+
+namespace SethCS.IO
+{
+    public static class DirectoryHelpers
+    {
+        public static void EnsureDirectoryExists( string path )
+        {
+            if( Directory.Exists( path ) == false )
+            {
+                Directory.CreateDirectory( path );
+            }
+        }
+    }
+}
