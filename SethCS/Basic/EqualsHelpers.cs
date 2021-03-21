dc2d8886afd75c021a4f00d8b4dfa4b97ee3dcd0
Seth Hendrick <seth@shendrick.net>
2021-03-21 13:18:00 -0400

    Added EqualsHelpers.ArePropertiesEqual.

---
+++ SethCS/Basic/EqualsHelpers.cs
@@ -0,0 +1,97 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Reflection;
+
+namespace SethCS.Basic
+{
+    [AttributeUsage( AttributeTargets.Property, AllowMultiple = false, Inherited = true )]
+    public class EqualsIgnoreAttribute : Attribute
+    {
+        // ---------------- Constructor ----------------
+
+        public EqualsIgnoreAttribute() :
+            this( true )
+        {
+        }
+
+        public EqualsIgnoreAttribute( bool shouldIgnore )
+        {
+            this.ShouldIgnore = shouldIgnore;
+        }
+
+        // ---------------- Properties ----------------
+
+        public bool ShouldIgnore { get; set; }
+    }
+
+    public static class EqualsHelpers
+    {
+        public static bool ArePropertiesEqual<T>( T left, T right )
+        {
+            if( ReferenceEquals( left, right ) )
+            {
+                return true;
+            }
+            else if( ReferenceEquals( left, null ) )
+            {
+                return ReferenceEquals( null, right );
+            }
+            else if( ReferenceEquals( null, right ) )
+            {
+                return ReferenceEquals( left, null );
+            }
+
+            bool areEqual = true;
+            foreach( PropertyInfo property in typeof( T ).GetProperties() )
+            {
+                EqualsIgnoreAttribute ignore = property.GetCustomAttribute<EqualsIgnoreAttribute>();
+                if( ( ignore != null ) && ignore.ShouldIgnore )
+                {
+                    continue;
+                }
+
+                object leftValue = property.GetValue( left );
+                object rightValue = property.GetValue( right );
+                if( ReferenceEquals( leftValue, null ) )
+                {
+                    areEqual &= ReferenceEquals( null, rightValue );
+                }
+                else
+                {
+                    areEqual &= leftValue.Equals( rightValue );
+                }
+            }
+
+            return areEqual;
+        }
+
+        public static bool OperatorDoubleEqualsHelper<T>( T left, T right )
+        {
+            if( ReferenceEquals( left, right ) )
+            {
+                return true;
+            }
+            else if( ReferenceEquals( left, null ) )
+            {
+                return ReferenceEquals( null, right );
+            }
+            else if( ReferenceEquals( null, right ) )
+            {
+                return ReferenceEquals( left, null );
+            }
+
+            return left.Equals( right );
+        }
+
+        public static bool OperatorNotEqualsHelper<T>( T left, T right )
+        {
+            return OperatorDoubleEqualsHelper( left, right ) == false;
+        }
+    }
+}
---
+++ Tests/Basic/EqualsHelpersTest.cs
@@ -0,0 +1,174 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using NUnit.Framework;
+using SethCS.Basic;
+
+namespace Tests.Basic
+{
+    [TestFixture]
+    public class EqualsHelpersTest
+    {
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void AreEqualTest()
+        {
+            TestClass uut1 = new TestClass
+            {
+                IgnoredIntProp = 2,
+                IntProp = 1,
+                StringProp = "Hello"
+            };
+
+            TestClass uut2 = new TestClass( uut1 );
+
+            this.ExepctEquals( uut1, uut2 );
+
+            // Change ignored prop, should still equal since its ignore.
+            uut2.IgnoredIntProp = uut1.IgnoredIntProp + 1;
+            this.ExepctEquals( uut1, uut2 );
+            uut2 = new TestClass( uut1 );
+
+            // Change any other property, should report false.
+            uut2.IntProp = uut1.IntProp + 1;
+            this.ExepctNotEquals( uut1, uut2 );
+            uut2 = new TestClass( uut1 );
+
+            uut2.StringProp= uut1.StringProp + 1;
+            this.ExepctNotEquals( uut1, uut2 );
+            uut2 = new TestClass( uut1 );
+        }
+
+        [Test]
+        public void NullParamTest()
+        {
+            TestClass uut = new TestClass
+            {
+                IgnoredIntProp = 2,
+                IntProp = 1,
+                StringProp = "Hello"
+            };
+
+            Assert.IsFalse( null == uut );
+            Assert.IsFalse( uut == null );
+            Assert.IsTrue( null != uut );
+            Assert.IsTrue( uut != null );
+            Assert.IsFalse( uut.Equals( null ) );
+        }
+
+        [Test]
+        public void SameReferenceTest()
+        {
+            TestClass uut = new TestClass
+            {
+                IgnoredIntProp = 2,
+                IntProp = 1,
+                StringProp = "Hello"
+            };
+
+            this.ExepctEquals( uut, uut );
+        }
+
+        [Test]
+        public void NullPropertyTest()
+        {
+            TestClass uut1 = new TestClass
+            {
+                IgnoredIntProp = 2,
+                IntProp = 1,
+                StringProp = null
+            };
+
+            TestClass uut2 = new TestClass( uut1 );
+
+            this.ExepctEquals( uut1, uut2 );
+
+            uut2.StringProp = "Not Null";
+            this.ExepctNotEquals( uut1, uut2 );
+            uut2 = new TestClass( uut1 );
+        }
+
+        // ---------------- Test Helpers ----------------
+
+        public void ExepctEquals( TestClass left, TestClass right )
+        {
+            Assert.IsTrue( left.Equals( right ) );
+            Assert.IsTrue( right.Equals( left ) );
+            Assert.IsTrue( left == right );
+            Assert.IsTrue( right == left );
+            Assert.IsFalse( left != right );
+            Assert.IsFalse( right != left );
+        }
+
+        public void ExepctNotEquals( TestClass left, TestClass right )
+        {
+            Assert.IsFalse( left.Equals( right ) );
+            Assert.IsFalse( right.Equals( left ) );
+            Assert.IsFalse( left == right );
+            Assert.IsFalse( right == left );
+            Assert.IsTrue( left != right );
+            Assert.IsTrue( right != left );
+        }
+
+        // ---------------- Helper Classes ----------------
+
+        public class TestClass : IEquatable<TestClass>
+        {
+            // ---------------- Constructor ----------------
+
+            public TestClass()
+            {
+            }
+
+            public TestClass( TestClass other )
+            {
+                this.IntProp = other.IntProp;
+                this.StringProp = other.StringProp;
+                this.IgnoredIntProp = other.IgnoredIntProp;
+            }
+
+            // ---------------- Properties ----------------
+
+            public int IntProp { get; set; }
+
+            [EqualsIgnore( false )]
+            public string StringProp { get; set; }
+
+            [EqualsIgnore]
+            public int IgnoredIntProp { get; set; }
+
+            // ---------------- Properties ----------------
+
+            public override bool Equals( object obj )
+            {
+                return Equals( obj as TestClass );
+            }
+
+            public bool Equals( TestClass other )
+            {
+                return EqualsHelpers.ArePropertiesEqual( this, other );
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+
+            public static bool operator ==( TestClass left, TestClass right )
+            {
+                return EqualsHelpers.OperatorDoubleEqualsHelper( left, right );
+            }
+
+            public static bool operator !=( TestClass left, TestClass right )
+            {
+                return EqualsHelpers.OperatorNotEqualsHelper( left, right );
+            }
+        }
+    }
+}
