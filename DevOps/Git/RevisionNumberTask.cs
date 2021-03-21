b01e1a70d0d6016bb540354f6e79e81268a065aa
Seth Hendrick <seth@shendrick.net>
2021-03-21 15:26:13 -0400

    Added git revisions.

---
+++ DevOps/Git/RevisionNumberTask.cs
@@ -0,0 +1,29 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using Cake.Frosting;
+using Seth.CakeLib.Git;
+using Seth.CakeLib.Git.RevisionNumber;
+
+namespace DevOps.Git
+{
+    [TaskName( "print_git_revision" )]
+    [TaskDescription( "Prints the number of commits on the current branch." )]
+    public sealed class RevisionNumberTask : DevOpsTask
+    {
+        public override void Run( BuildContext context )
+        {
+            GitToolSettings toolSettings = new GitToolSettings
+            {
+                WorkingDirectory = context.RepoRoot
+            };
+
+            GitRevisionNumberRunner runner = new GitRevisionNumberRunner( context, toolSettings );
+            runner.Run();
+        }
+    }
+}
---
+++ Seth.CakeLib/Git/RevisionNumber/GitRevisionNumberConfig.cs
@@ -0,0 +1,16 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using Cake.ArgumentBinder;
+
+namespace Seth.CakeLib.Git.RevisionNumber
+{
+    public sealed class GitRevisionNumberConfig : BaseGitQueryTask
+    {
+    }
+}
