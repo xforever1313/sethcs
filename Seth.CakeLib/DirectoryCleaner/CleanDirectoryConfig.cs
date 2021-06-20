ee9e4134f6547aa41aaccf588b3145f78941ee1e
Seth Hendrick <seth@shendrick.net>
2021-06-20 16:08:11 -0400

    Added DirectoryCleaner tasks.

---
+++ Seth.CakeLib/DirectoryCleaner/CleanDirectoryConfig.cs
@@ -0,0 +1,38 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using Cake.ArgumentBinder;
+using Cake.Core.IO;
+
+namespace Seth.CakeLib.DirectoryCleaner
+{
+    public class CleanDirectoryConfig
+    {
+        // ---------------- Constructor ----------------
+
+        public CleanDirectoryConfig()
+        {
+        }
+
+        // ---------------- Properties ----------------
+
+        [DirectoryPathArgument(
+            "path",
+            Description = "Path to the directory to clean.  It will be created if it does not exist",
+            Required = true,
+            MustExist = false
+        )]
+        public DirectoryPath Path { get; set; }
+
+        // ---------------- Functions ----------------
+
+        public override string ToString()
+        {
+            return ArgumentBinder.ConfigToStringHelper( this );
+        }
+    }
+}
---
+++ Seth.CakeLib/DirectoryCleaner/CleanDirectoryTask.cs
@@ -0,0 +1,30 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using Cake.ArgumentBinder;
+using Cake.Common.Diagnostics;
+using Cake.Common.IO;
+using Cake.Core;
+using Cake.Frosting;
+
+namespace Seth.CakeLib.DirectoryCleaner
+{
+    public class CleanDirectoryTask : FrostingTask
+    {
+        // ---------------- Functions ----------------
+
+        public override void Run( ICakeContext context )
+        {
+            CleanDirectoryConfig config = context.CreateFromArguments<CleanDirectoryConfig>();
+
+            context.Information( config );
+
+            context.EnsureDirectoryExists( config.Path );
+            context.CleanDirectory( config.Path );
+        }
+    }
+}
