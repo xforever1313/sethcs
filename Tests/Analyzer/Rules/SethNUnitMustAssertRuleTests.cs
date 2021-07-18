1d595b87d48af662407d730ac7ae74cdd8967061
Seth Hendrick <seth@shendrick.net>
2021-07-18 15:36:51 -0400

    Added more unit tests, but it is starting to seem like what we are trying to do is impossible.

--- Tests/Analyzer/Rules/SethNUnitMustAssertRuleTests.cs
+++ Tests/Analyzer/Rules/SethNUnitMustAssertRuleTests.cs
@@ -37,8 +37,11 @@ namespace Tests.Analyzer.Rules
 
         // ---------------- Tests ----------------
 
+        /// <summary>
+        /// If we don't call Assert, we should get a warning.
+        /// </summary>
         [Test]
-        public async Task NoWarningTest()
+        public async Task NoAssertCallTest()
         {
             string test =
 @"
@@ -47,6 +50,37 @@ using NUnit.Framework;
 
 namespace TestNamespace
 {
+    [TestFixture]
+    public class SomeFixture
+    {
+        [Test]
+        public void {|#0:SomeTest|}()
+        {
+        }
+    }
+}
+";
+            var expected = VerifyCS.Diagnostic( SethNUnitMustAssertRule.Rule )
+                .WithLocation( 0 )
+                .WithSeverity( DiagnosticSeverity.Warning )
+                .WithArguments( "SomeTest" );
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies, expected );
+        }
+
+        /// <summary>
+        /// If we invoke Assert in the method of the test, we should not get a warning.
+        /// </summary>
+        [Test]
+        public async Task AssertInTestBodyTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    [TestFixture]
     public class SomeFixture
     {
         [Test]
@@ -60,6 +94,10 @@ namespace TestNamespace
             await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
         }
 
+        /// <summary>
+        /// If we call Assert within the a method of the same class,
+        /// we should get no warning.
+        /// </summary>
         [Test]
         public async Task AssertCallWithCallStackTest()
         {
@@ -70,6 +108,7 @@ using NUnit.Framework;
 
 namespace TestNamespace
 {
+    [TestFixture]
     public class SomeFixture
     {
         [Test]
@@ -88,8 +127,13 @@ namespace TestNamespace
             await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
         }
 
+        /// <summary>
+        /// If an instance class calls assert, we should
+        /// not get a warning.
+        /// </summary>
+        /// <returns></returns>
         [Test]
-        public async Task NoAssertCallTest()
+        public async Task InstanceAssertCallTest()
         {
             string test =
 @"
@@ -98,20 +142,193 @@ using NUnit.Framework;
 
 namespace TestNamespace
 {
+    [TestFixture]
     public class SomeFixture
     {
         [Test]
-        public void {|#0:SomeTest|}()
+        public void SomeTest()
         {
+            ClassA a = new ClassA();
+            a.CallAssert();
+        }
+    }
+
+    public class ClassA
+    {
+        public void CallAssert()
+        {
+            Assert.Pass( ""Passed!"" );
         }
     }
 }
 ";
-            var expected = VerifyCS.Diagnostic( SethNUnitMustAssertRule.Rule )
-                .WithLocation( 0 )
-                .WithSeverity( DiagnosticSeverity.Warning )
-                .WithArguments( "SomeTest" );
-            await VerifyCS.VerifyAnalyzerAsync( test, assemblies, expected );
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
+        }
+
+        /// <summary>
+        /// Ensure if we call assert in an override method,
+        /// but call that class that overrides, we get no warning.
+        /// </summary>
+        [Test]
+        public async Task OverrideAndInvokeChildClassTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    [TestFixture]
+    public class SomeFixture
+    {
+        [Test]
+        public void SomeTest()
+        {
+            ClassB a = new ClassB();
+            a.CallAssert();
+        }
+    }
+
+    public class ClassA
+    {
+        public virtual void CallAssert()
+        {
+        }
+    }
+
+    public class ClassB : ClassA
+    {
+        public override void CallAssert()
+        {
+            Assert.Pass( ""Passed!"" );
+        }
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
+        }
+
+        /// <summary>
+        /// Ensure if we call assert in an override method,
+        /// but the type is the base type, we don't get the warning.
+        /// </summary>
+        [Test]
+        public async Task OverrideAndInvokeBaseClassTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    [TestFixture]
+    public class SomeFixture
+    {
+        [Test]
+        public void SomeTest()
+        {
+            ClassA a = new ClassB();
+            a.CallAssert();
+        }
+    }
+
+    public class ClassA
+    {
+        public virtual void CallAssert()
+        {
+        }
+    }
+
+    public class ClassB : ClassA
+    {
+        public override void CallAssert()
+        {
+            Assert.Pass( ""Passed!"" );
+        }
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
+        }
+
+        /// <summary>
+        /// If we call Assert through an action, we
+        /// should not get a warning.
+        /// </summary>
+        [Test]
+        public async Task AssertThroughActionTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    [TestFixture]
+    public class SomeFixture
+    {
+        [Test]
+        public void SomeTest()
+        {
+            Action assertAction = new Action(
+                () =>
+                {
+                    Assert.Pass( ""Success!"" );
+                }
+            );
+
+             assertAction();
+        }
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
+        }
+
+        /// <summary>
+        /// If we call Assert through an action on a property, we
+        /// should not get a warning.
+        /// </summary>
+        [Test]
+        public async Task AssertThroughPropertyFuncTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    [TestFixture]
+    public class SomeFixture
+    {
+        [Test]
+        public void SomeTest()
+        {
+            ClassA a = new ClassA();
+            a.AssertFunc();
+        }
+    }
+
+    public class ClassA
+    {
+        public ClassA()
+        {
+            this.AssertFunc = delegate ()
+            {
+                Assert.Pass( ""Success!"" );
+                return true;
+            };
+        }
+
+        public Func<bool> AssertFunc { get; private set; }
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
         }
     }
 }
