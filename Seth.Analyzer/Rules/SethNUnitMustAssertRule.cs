e0c726287676cf7f126dfd267c1872d522c363f0
Seth Hendrick <seth@shendrick.net>
2021-07-18 13:57:10 -0400

    Added the AssertRule that can go down the stack.

---
+++ Seth.Analyzer/Rules/SethNUnitMustAssertRule.cs
@@ -0,0 +1,171 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Seth.Analyzer.Rules
+{
+    public static class SethNUnitMustAssertRule
+    {
+        // ---------------- Fields ----------------
+
+        private const string Descriptor = nameof( SethNUnitMustAssertRule );
+
+        private static readonly LocalizableString Title = "NUnit tests must call Assert";
+        private static readonly LocalizableString MessageFormat = "Assert must be called at least once in test '{0}'.";
+        private static readonly LocalizableString Description =
+            "Assert should be called at least once in an NUnit test to ensure we actually test for something.";
+
+        private static readonly string RuleCategory = DiagnosticCategory.Warning.ToString();
+
+        private const DiagnosticSeverity Serverity = DiagnosticSeverity.Warning;
+
+        // ---------------- Constructor ----------------
+
+        static SethNUnitMustAssertRule()
+        {
+            Rule = new DiagnosticDescriptor(
+                Descriptor,
+                Title,
+                MessageFormat,
+                RuleCategory.ToString(),
+                Serverity,
+                isEnabledByDefault: true,
+                description: Description
+            );
+        }
+
+        // ---------------- Properties ----------------
+
+        public static DiagnosticDescriptor Rule { get; private set; }
+
+        // ---------------- Functions ----------------
+
+        public static void Init( AnalysisContext context )
+        {
+            context.RegisterOperationAction( Run, OperationKind.MethodBody );
+        }
+
+        private static void Run( OperationAnalysisContext context )
+        {
+            ISymbol symbol = context.ContainingSymbol;
+            if( symbol is IMethodSymbol == false )
+            {
+                return;
+            }
+
+            IMethodSymbol testMethod = (IMethodSymbol)symbol;
+            var attributes = testMethod.GetAttributes();
+            if( attributes.Length == 0 )
+            {
+                return;
+            }
+
+            bool foundAttribute = false;
+            foreach( AttributeData attribute in attributes )
+            {
+                INamedTypeSymbol attributeSymbol = attribute.AttributeClass;
+                if( attributeSymbol == null )
+                {
+                    continue;
+                }
+
+                INamespaceSymbol namespaceSymbol = attributeSymbol.ContainingNamespace;
+                if( namespaceSymbol == null )
+                {
+                    continue;
+                }
+
+                if(
+                    ( "NUnit.Framework".Equals( namespaceSymbol.ToString() ) ) &&
+                    ( "TestAttribute".Equals( attributeSymbol.Name ) )
+                )
+                {
+                    foundAttribute = true;
+                    break;
+                }
+            }
+
+            if( foundAttribute == false )
+            {
+                return;
+            }
+
+            Stack<IOperation> operations = new Stack<IOperation>();
+            operations.Push( context.Operation );
+
+            while( operations.Count != 0 )
+            {
+                IOperation currentOp = operations.Pop();
+                if( IsAssertInvocation( currentOp, operations ) )
+                {
+                    return;
+                }
+
+                foreach( IOperation child in currentOp.Children )
+                {
+                    operations.Push( child );
+                }
+            }
+
+            var diagnostic = Diagnostic.Create(
+                Rule,
+                testMethod.Locations[0],
+                testMethod.Name ?? string.Empty
+            );
+            context.ReportDiagnostic( diagnostic );
+        }
+
+        private static bool IsAssertInvocation( IOperation operation, Stack<IOperation> operations )
+        {
+            if( operation is IInvocationOperation invokeOp )
+            {
+                IMethodSymbol invocationMethod = invokeOp.TargetMethod;
+                if( invocationMethod != null )
+                {
+                    INamedTypeSymbol classType = invocationMethod.ContainingType;
+                    if( "NUnit.Framework.Assert".Equals( classType.ToString() ) )
+                    {
+                        return true;
+                    }
+                }
+
+                foreach( Location location in invocationMethod.Locations )
+                {
+                    SyntaxTree tree = location.SourceTree;
+                    if( tree == null )
+                    {
+                        continue;
+                    }
+
+                    if( tree.TryGetRoot( out SyntaxNode root ) && ( root != null ) )
+                    {
+                        SyntaxNode operationNode = root.FindNode( location.SourceSpan );
+                        if( operationNode == null )
+                        {
+                            continue;
+                        }
+
+                        IOperation callingOp = operation.SemanticModel?.GetOperation( operationNode );
+                        if( callingOp != null )
+                        {
+                            operations.Push( callingOp );
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/Analyzer/Rules/SethNUnitMustAssertRuleTests.cs
@@ -0,0 +1,117 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using NUnit.Framework;
+using Seth.Analyzer.Rules;
+using SethCS.Basic;
+using VerifyCS = Tests.Analyzer.CSharpCodeFixVerifier<
+    Seth.Analyzer.SethCodeAnalyzer,
+    Seth.Analyzer.SethCodeFixProvider
+>;
+
+namespace Tests.Analyzer.Rules
+{
+    [TestFixture]
+    public sealed class SethNUnitMustAssertRuleTests
+    {
+        // ---------------- Fields ----------------
+
+        private static IEnumerable<Assembly> assemblies = new List<Assembly>
+        {
+            typeof( TestFixtureAttribute ).Assembly
+        }.AsReadOnly();
+
+        // ---------------- Tests ----------------
+
+        [Test]
+        public async Task NoWarningTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    public class SomeFixture
+    {
+        [Test]
+        public void SomeTest()
+        {
+            Assert.Pass( ""Success"" );
+        }
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
+        }
+
+        [Test]
+        public async Task AssertCallWithCallStackTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    public class SomeFixture
+    {
+        [Test]
+        public void SomeTest()
+        {
+            CallAssert();
+        }
+
+        private void CallAssert()
+        {
+            Assert.Pass( ""Success!"" );
+        }
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies );
+        }
+
+        [Test]
+        public async Task NoAssertCallTest()
+        {
+            string test =
+@"
+using System;
+using NUnit.Framework;
+
+namespace TestNamespace
+{
+    public class SomeFixture
+    {
+        [Test]
+        public void {|#0:SomeTest|}()
+        {
+        }
+    }
+}
+";
+            var expected = VerifyCS.Diagnostic( SethNUnitMustAssertRule.Rule )
+                .WithLocation( 0 )
+                .WithSeverity( DiagnosticSeverity.Warning )
+                .WithArguments( "SomeTest" );
+            await VerifyCS.VerifyAnalyzerAsync( test, assemblies, expected );
+        }
+    }
+}
