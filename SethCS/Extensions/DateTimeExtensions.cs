d3026e0ddbc2f0441f4389f3f21c1ed1956c056b
Seth Hendrick <seth@shendrick.net>
2021-12-23 15:43:46 -0500

    Added format strings so they can be referenced elsewhere.

--- SethCS/Extensions/DateTimeExtensions.cs
+++ SethCS/Extensions/DateTimeExtensions.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 //          Copyright Seth Hendrick 2015-2021.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
@@ -11,6 +11,14 @@ namespace SethCS.Extensions
 {
     public static class DateTimeExtensions
     {
+        // ---------------- Fields ----------------
+
+        public static readonly string TimeStampFormatString = "O";
+
+        public static readonly string FileNameStringFormat = "yyyy-MM-dd_HH-mm-ss-ffff";
+
+        // ---------------- Functions ----------------
+
         /// <summary>
         /// Returns a string of the given timestamp in the form of ISO 8601
         /// The same as DateTime.ToString("o");
@@ -19,7 +27,7 @@ namespace SethCS.Extensions
         /// <returns>String representation of the timestamp in ISO 8601 format.</returns>
         public static string ToTimeStampString( this DateTime timeStamp )
         {
-            return timeStamp.ToString( "O" );
+            return timeStamp.ToString( TimeStampFormatString );
         }
 
         /// <summary>
@@ -30,7 +38,7 @@ namespace SethCS.Extensions
         /// <returns>A string in the form of yyyy-MM-dd_HH-mm-ss-ffff</returns>
         public static string ToFileNameString( this DateTime timeStamp )
         {
-            return timeStamp.ToString( "yyyy-MM-dd_HH-mm-ss-ffff" );
+            return timeStamp.ToString( FileNameStringFormat );
         }
     }
 }
