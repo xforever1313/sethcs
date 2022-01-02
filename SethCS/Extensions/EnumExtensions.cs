811500c058367c39b11c17ad0c319b776896283c
Seth Hendrick <seth@shendrick.net>
2022-01-02 13:46:47 -0500

    Added extensions methods to Enums so they can be sorted by their numeric value easier.

---
+++ SethCS/Extensions/EnumExtensions.cs
@@ -0,0 +1,44 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SethCS.Extensions
+{
+    public static class EnumExtensions
+    {
+        // ---------------- Functions ----------------
+
+        /// <summary>
+        /// Sorts the enums by their numeric values (starting from the lowest)
+        /// and returns them.
+        /// </summary>
+        public static IEnumerable<TEnum> SortByNumber<TEnum>()
+            where TEnum : Enum
+        {
+            return
+                Enum.GetValues( typeof( TEnum ) )
+                    .OfType<TEnum>()
+                    .OrderBy( x => x );
+        }
+
+        /// <summary>
+        /// Sorts the enums by their numeric values (starting from the highest)
+        /// and returns them.
+        /// </summary>
+        public static IEnumerable<TEnum> SortByNumberDecending<TEnum>()
+            where TEnum : Enum
+        {
+            return
+                Enum.GetValues( typeof( TEnum ) )
+                    .OfType<TEnum>()
+                    .OrderByDescending( x => x );
+        }
+    }
+}
---
+++ Tests/Extensions/EnumExtensionTests.cs
@@ -0,0 +1,95 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Linq;
+using NUnit.Framework;
+using SethCS.Extensions;
+
+namespace Tests.Extensions
+{
+    [TestFixture]
+    public sealed class EnumExtensionTests
+    {
+        // ---------------- Enums ----------------
+
+        private enum OrderedEnum
+        {
+            Negative1 = -1,
+            Zero = 0,
+            One = 1,
+            Two = 2,
+            Three = 3
+        }
+
+        public enum UnorderedEnum
+        {
+            One = 1,
+            Three = 3,
+            Two = 2,
+            Zero = 0,
+            Negative1 = -1
+        }
+
+        // ---------------- Tets ----------------
+
+        [Test]
+        public void SortByNumber_OrderedEnumTest()
+        {
+            // Act
+            OrderedEnum[] list = EnumExtensions.SortByNumber<OrderedEnum>().ToArray();
+
+            // Check
+            Assert.AreEqual( OrderedEnum.Negative1, list[0] );
+            Assert.AreEqual( OrderedEnum.Zero, list[1] );
+            Assert.AreEqual( OrderedEnum.One, list[2] );
+            Assert.AreEqual( OrderedEnum.Two, list[3] );
+            Assert.AreEqual( OrderedEnum.Three, list[4] );
+        }
+
+        [Test]
+        public void SortByNumber_UnorderedEnumTest()
+        {
+            // Act
+            UnorderedEnum[] list = EnumExtensions.SortByNumber<UnorderedEnum>().ToArray();
+
+            // Check
+            Assert.AreEqual( UnorderedEnum.Negative1, list[0] );
+            Assert.AreEqual( UnorderedEnum.Zero, list[1] );
+            Assert.AreEqual( UnorderedEnum.One, list[2] );
+            Assert.AreEqual( UnorderedEnum.Two, list[3] );
+            Assert.AreEqual( UnorderedEnum.Three, list[4] );
+        }
+
+        [Test]
+        public void SortByNumberDecending_OrderedEnumTest()
+        {
+            // Act
+            OrderedEnum[] list = EnumExtensions.SortByNumberDecending<OrderedEnum>().ToArray();
+
+            // Check
+            Assert.AreEqual( OrderedEnum.Negative1, list[4] );
+            Assert.AreEqual( OrderedEnum.Zero, list[3] );
+            Assert.AreEqual( OrderedEnum.One, list[2] );
+            Assert.AreEqual( OrderedEnum.Two, list[1] );
+            Assert.AreEqual( OrderedEnum.Three, list[0] );
+        }
+
+        [Test]
+        public void SortByNumberDecending_UnorderedEnumTest()
+        {
+            // Act
+            UnorderedEnum[] list = EnumExtensions.SortByNumberDecending<UnorderedEnum>().ToArray();
+
+            // Check
+            Assert.AreEqual( UnorderedEnum.Negative1, list[4] );
+            Assert.AreEqual( UnorderedEnum.Zero, list[3] );
+            Assert.AreEqual( UnorderedEnum.One, list[2] );
+            Assert.AreEqual( UnorderedEnum.Two, list[1] );
+            Assert.AreEqual( UnorderedEnum.Three, list[0] );
+        }
+    }
+}
