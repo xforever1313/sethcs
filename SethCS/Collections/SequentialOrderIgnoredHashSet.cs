3adc5463fef64287828c269e348b8bd0838890b9
Seth Hendrick <seth@shendrick.net>
2025-03-30 17:01:57 -0400

    Added HashSet that has order ignored.

---
+++ SethCS/Collections/SequentialOrderIgnoredHashSet.cs
@@ -0,0 +1,83 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using SethCS.Basic;
+
+namespace SethCS.Collections
+{
+    /// <summary>
+    /// Version of <see cref="HashSet{T}"/> where the Equals method
+    /// invokes SequenceEquals instead of doing a reference equals.
+    /// Order is also ignored.
+    /// </summary>
+    public class SequentialOrderIgnoredHashSet<T> : HashSet<T>, IEquatable<SequentialOrderIgnoredHashSet<T>>
+    {
+        // ---------------- Constructor ----------------
+
+        public SequentialOrderIgnoredHashSet() :
+            base( new EqualityComparer() )
+        {
+        }
+
+        // ---------------- Methods ----------------
+
+        public override bool Equals( object? obj )
+        {
+            return Equals( obj as SequentialOrderIgnoredHashSet<T> );
+        }
+
+        public bool Equals( SequentialOrderIgnoredHashSet<T>? other )
+        {
+            if( other is null )
+            {
+                return false;
+            }
+
+            if( this.Count != other.Count )
+            {
+                return false;
+            }
+
+            return Enumerable.SequenceEqual(
+                this.OrderBy( t => t ),
+                other.OrderBy( t => t )
+            );
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+            foreach( T value in this.OrderBy( t => t ) )
+            {
+                hashCode = HashCode.Combine( value );
+            }
+
+            return hashCode;
+        }
+
+        // ---------------- Helper Classes ----------------
+
+        private sealed class EqualityComparer : IEqualityComparer<T?>
+        {
+            public bool Equals( T? x, T? y )
+            {
+                return EqualsHelpers.OperatorDoubleEqualsHelper( x, y );
+            }
+
+            public int GetHashCode( [DisallowNull] T? obj )
+            {
+                return obj.GetHashCode();
+            }
+        }
+    }
+}
---
+++ Tests/Collections/SequentialOrderIgnoredHashSetTests.cs
@@ -0,0 +1,96 @@
+﻿//
+//          Copyright Seth Hendrick 2015-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using NUnit.Framework;
+using SethCS.Collections;
+
+namespace Tests.Collections
+{
+    [TestFixture]
+    public sealed class SequentialOrderIgnoredHashSetTests
+    {
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void EmptyIsEqualTest()
+        {
+            // Setup
+            var uut1 = new SequentialOrderIgnoredHashSet<int>();
+            var uut2 = new SequentialOrderIgnoredHashSet<int>();
+
+            // Act
+            bool areEqual = uut1.Equals( uut2 );
+
+            // Check
+            Assert.IsTrue( areEqual );
+            Assert.AreEqual( uut1.GetHashCode(), uut2.GetHashCode() );
+        }
+
+        [Test]
+        public void EqualSameOrderTest()
+        {
+            // Setup
+            var uut1 = new SequentialOrderIgnoredHashSet<int>
+            {
+                1, 2, 3
+            };
+            var uut2 = new SequentialOrderIgnoredHashSet<int>()
+            {
+                1, 2, 3
+            };
+
+            // Act
+            bool areEqual = uut1.Equals( uut2 );
+
+            // Check
+            Assert.IsTrue( areEqual );
+            Assert.AreEqual( uut1.GetHashCode(), uut2.GetHashCode() );
+        }
+
+        [Test]
+        public void EqualDifferentOrderTest()
+        {
+            // Setup
+            var uut1 = new SequentialOrderIgnoredHashSet<int>
+            {
+                1, 3, 2
+            };
+            var uut2 = new SequentialOrderIgnoredHashSet<int>()
+            {
+                1, 2, 3
+            };
+
+            // Act
+            bool areEqual = uut1.Equals( uut2 );
+
+            // Check
+            Assert.IsTrue( areEqual );
+            Assert.AreEqual( uut1.GetHashCode(), uut2.GetHashCode() );
+        }
+
+        [Test]
+        public void EqualDuplicateEntriesTest()
+        {
+            // Setup
+            var uut1 = new SequentialOrderIgnoredHashSet<int>
+            {
+                1, 2, 3, 3
+            };
+            var uut2 = new SequentialOrderIgnoredHashSet<int>()
+            {
+                1, 2, 2, 3
+            };
+
+            // Act
+            bool areEqual = uut1.Equals( uut2 );
+
+            // Check
+            Assert.IsTrue( areEqual );
+            Assert.AreEqual( uut1.GetHashCode(), uut2.GetHashCode() );
+        }
+    }
+}
